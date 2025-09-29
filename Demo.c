d41fc94efb67135d90bb6da28940ded6194a8b82
Emran <mdemranhossainnub@gmail.com>
2025-09-30 01:59:48 +0600

    The second commit

--- Demo.c
+++ Demo.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
-  printf("I am Emran!");
+  printf("Hello World!\n");
+  printf("I am Emran!\n");
+  printf("I am learning C programming language.\n");
   return 0;
 }
